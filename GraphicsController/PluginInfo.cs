2f263db9ea1696362100c5646dcb80dcb47e2601
g00berguy <sadsdadsasdsdsadsd@gmail.com>
2023-08-11 22:42:07 -0700

    commit
    forgot to change version lol

--- GraphicsController/PluginInfo.cs
+++ GraphicsController/PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goober.graphicscontroller";
         public const string Name = "Graphics Controller";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
 }
